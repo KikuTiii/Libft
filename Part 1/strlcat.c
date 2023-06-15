2e909a2cb37176eaf5d6f6ce18da9470e2320bae
Matheus Kikuti <111128991+KikuTiii@users.noreply.github.com>
2023-06-14 23:47:03 -0300

    Update strlcat.c

--- Part 1/strlcat.c
+++ Part 1/strlcat.c
@@ -1,27 +1,27 @@
-#include "libft.h"
+#include"libft.h"
 
-unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
+unsigned int	ft_strlcat(char *dest, const char *src, unsigned int size)
 {
-    unsigned char count;
-    unsigned char len_src;
-    unsigned char len_dest;
+	unsigned char	count;
+	unsigned char	len_src;
+	unsigned char	len_dest;
 
-    if (size == 0 && dest == 0)
-        return (0);
-    count = 0;
-    len_src = ft_strlen(src);
-    len_dest = ft_strlen(dest);
-    if (size > len_dest)
-    {
-        while (src[count] != 0 && count + len_dest < size - 1)
-        {
-            dest[count + len_dest] = src[count];
-            count++;
-        }
-        dest[count + len_dest] = 0;
-        return (len_src + len_dest);
-    }
-    return (len_src + size);
+	if (size == 0 && dest == 0)
+		return (0);
+	count = 0;
+	len_src = ft_strlen(src);
+	len_dest = ft_strlen(dest);
+	if (size > len_dest)
+	{
+		while (src[count] != 0 && count + len_dest < size -1)
+		{
+			dest[count + len_dest] = src[count];
+			count++;
+		}
+		dest[count + len_dest] = 0;
+		return (len_src + len_dest);
+	}
+	return (len_src + size);
 }
 
 // #include <stdio.h>
