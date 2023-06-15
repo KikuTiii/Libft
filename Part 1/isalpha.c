62bad7b2001c43aa264200af4edd6cb22c582be9
Matheus Kikuti <111128991+KikuTiii@users.noreply.github.com>
2023-06-14 23:41:36 -0300

    Update isalpha.c

--- Part 1/isalpha.c
+++ Part 1/isalpha.c
@@ -1,14 +1,8 @@
-#include <unistd.h>
-#include <stdio.h>
+#include "libft.h"
 
-int ft_isalpha(char c)
+int	ft_isalpha(int c)
 {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-    {
-        return (1);
-    }
-    else
-        return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
 
 // int main(void)
