e8c04b7f93783860ace704fcf4bcd0363c455dd6
Matheus Kikuti <111128991+KikuTiii@users.noreply.github.com>
2023-06-14 23:41:11 -0300

    Update isalnum.c

--- Part 1/isalnum.c
+++ Part 1/isalnum.c
@@ -1,14 +1,7 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int ft_isalnum(int c)
+int	ft_isalnum(int c)
 {
-    if ((c >= 0 && c <= 9) || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-    {
-        return (1);
-    }
-    else
-        return (0);
+	return ((c >= '0' && c <= '9')
+		|| ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')));
 }
 
 // int main(void)
